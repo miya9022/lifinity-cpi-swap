ac10c57800f508f34ffc975c2f1cce2aa156a59b
killer <miya9022@gmail.com>
2025-03-02 00:20:15 +0700

    Make declaration for discriminator bytes

--- programs/lifinity_cpi_swap/src/states.rs
+++ programs/lifinity_cpi_swap/src/states.rs
@@ -1,6 +1,8 @@
 use anchor_lang::prelude::*;
 
-#[account(discriminator = [143, 245, 200, 17, 74, 214, 196, 135])]
+const LIFINITY_DISCRIMINATOR: [u8; 8] = [143, 245, 200, 17, 74, 214, 196, 135];
+
+#[account(discriminator = LIFINITY_DISCRIMINATOR)]
 pub struct LifinityPool {
 	pub initializer_key: Pubkey,
 	pub initializer_deposit_token_account: Pubkey,
